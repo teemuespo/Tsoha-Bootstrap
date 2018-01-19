056e02ee2e3b25ef2c90bc82734d4c19fc740672
Espo Teemu A <tespo@da2-cs-z1-01.pc.helsinki.fi>
2018-01-19 15:21:18 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="tespo"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="tikaharjoitustyö"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
